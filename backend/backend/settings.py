0d26e964f0509771e4521d666349d9142229a224
LenarSag <saglenar@gmail.com>
2024-06-17 09:20:24 +0300

    updated env file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,15 +8,14 @@ including database settings, installed applications, middleware, and more.
 import os
 from pathlib import Path
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv("SECRET_KEY", "mysecret")
 
-SECRET_KEY = "django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^"
+DEBUG = False
 
-DEBUG = True
-
-ALLOWED_HOSTS = ["158.160.88.186", "127.0.0.1", "localhost", "leo-yandex.zapto.org"]
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")
 
 # Application definition
 
