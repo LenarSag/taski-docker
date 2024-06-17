cfc1f2281274ae116e340e6bc789ba82830a4cc1
LenarSag <saglenar@gmail.com>
2024-06-17 09:24:15 +0300

    updated env file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,6 @@ including database settings, installed applications, middleware, and more.
 import os
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "mysecret")
